3aacf8db2c4c64a09bac6d26107dadf31077b560
Diego_Perez <dyego79@gmail.com>
2023-11-17 20:29:50 -0300

    Correcciones para Deploy2

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -9,7 +9,7 @@ export default async function CustomersTable({
   customers: FormattedCustomersTable[];
 }) {
   return (
-    <div className="w-full">
+    <div className="w-full ">
       <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
         Customers
       </h1>
